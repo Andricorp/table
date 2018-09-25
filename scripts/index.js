d88fdb6dcb8676464450d363d5d5a634cb9701f1
Andricorp <nacigulev@gmail.com>
2018-09-25 17:47:58 +0300

    search

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,29 @@ let send = document.getElementById('get')
 let search = document.getElementById('search')
 let variants = document.getElementById('variants')
 let text = document.getElementById('text')
-let variant = document.createElement('div')
+// let variant = document.createElement('div')
 
 
 
 
 
-variant.style.background='gray';
-variant.style.opacity=0.8;
+
 
 function jsonPost(url, method)
     {
         return new Promise((resolve, reject) => {
-            var xhr = new XMLHttpRequest();   
-            xhr.onerror = () => reject(new Error('jsonPost failed'))
+            var x = new XMLHttpRequest();   
+            x.onerror = () => reject(new Error('jsonPost failed'))
             //x.setRequestHeader('Content-Type', 'application/json');
-            xhr.open(method, url, true);
+            x.open(method, url, true);
             // x.send(JSON.stringify(data))
-            xhr.send()
+            x.send()
 
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
-                    resolve(JSON.parse(xhr.responseText))
+            x.onreadystatechange = () => {
+                if (x.readyState == XMLHttpRequest.DONE && x.status == 200){
+                    resolve(JSON.parse(x.responseText))
                 }
-                else if (xhr.status != 200){
+                else if (x.status != 200){
                     reject(new Error('status is not 200'))
                 }
             }
@@ -34,55 +33,39 @@ function jsonPost(url, method)
     }
 
 
-let prev = '';
-function autocomplite(){    
-    let searchV = search.value
-    console.log('vjv',searchV);
-    jsonPost("http://api.tvmaze.com/search/shows?q="+searchV, 'GET')
-    .then(get=>{
-        console.log(prev);
-        prev.innerHTML = ""
-        // variant = document.createElement('div')
-        get.forEach((el, index) => {
-            console.log(el.show.name); 
-            let p = document.createElement('p')
-            p.style.cursor = 'pointer'
-            p.style.color='white'
-            p.addEventListener('click', function(){
-                // search.value = p.innerHTML
-                // variant.innerHTML='';
-                parseGet()
+    let prev = '';
+    search.oninput = function() {
+        let searchV = search.value
+        console.log('vjv',searchV);
+        jsonPost("http://api.tvmaze.com/search/shows?q="+searchV, 'GET')
+        .then(get=>{
+            // if(i>0){
+                console.log(prev); prev.innerHTML = ""
+            // }
+            variant = document.createElement('div')
+            get.forEach((el, index) => {
+                console.log(el.show.name); 
+                let p = document.createElement('p')
+                p.innerHTML = el.show.name;
+                variant.appendChild(p)
+                if(index>5)return
             })
-            p.innerHTML = el.show.name;
-            variant.appendChild(p)
-            if(index>5)return
+            variants.appendChild(variant)
+
+            prev = variant
+
+        
         })
-        variants.appendChild(variant)
-
-        prev = variant
-    })
-}
-typingCounter = 0
-// let typed = []
-search.oninput = function() {
-    // typingCounter++
-
-    // let t = setTimeout((typingCounter)=>typingCounter, 1000)    
-    // if( setTimeout((typingCounter)=>typingCounter, 1000) <typingCounter ){
-        autocomplite()
-    // }
-}
+      }
 
 
 
 
-function parseGet (searchV = search.value){
-    text.innerHTML = ''
-    variant.innerHTML = ''
+function Get (searchV = search.value){
+    
 
         jsonPost("http://api.tvmaze.com/search/shows?q="+searchV, 'GET')
         .then(get=>{
-            search.value = ""
             if(!get.length){
                 console.log('NO DATA');
                 let err = document.createElement('h1');
@@ -132,8 +115,8 @@ function parseGet (searchV = search.value){
 }
 
 
-parseGet('girls')
+Get('girls')
 
 send.addEventListener('click', function(){
-    parseGet()
+    Get()
 });
